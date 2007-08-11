2d56386f6e1152bd8e4c0b74a257a56aa804830d
Laurent Montel <montel@kde.org>
2007-08-11 09:09:01 +0000

    fix extract message
    svn path=/trunk/playground/games/ksirk/; revision=698844

--- ksirk/Messages.sh
+++ ksirk/Messages.sh
@@ -5,4 +5,6 @@ $EXTRACTRC `find . -name \*.ui -o -name \*.rc` > rc.cpp
 $EXTRACTRC --tag=desc `find skins -iname onu.xml` >> rc.cpp
 ./extract-onu-names.pl  `find skins -iname onu.xml` >> rc.cpp
 
-$XGETTEXT `find $SRCDIR -name "*.cpp" -o -name "*.h"` rc.cpp -o $podir/ksirk.pot
+$XGETTEXT `find $SRCDIR -name "*.cpp"` rc.cpp -o $podir/ksirk.pot
+rm -f rc.cpp
+
