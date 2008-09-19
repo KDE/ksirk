d59d71789da0cb3a7e0b136c30124112ad3c2032
Gaël de Chalendar <kleag@free.fr>
2008-09-19 21:19:36 +0000

    Merged from trunk, revision 862789: Correct strings extraction scripts
    svn path=/branches/KDE/4.1/kdegames/ksirk/; revision=862791

--- ksirk/Messages.sh
+++ ksirk/Messages.sh
@@ -1,9 +1,8 @@
 #! /usr/bin/env bash
 
 # process the .ui and .rc files
-$EXTRACTRC `find . -name \*.ui -o -name \*.rc` >> rc.cpp
-$EXTRACTRC --tag=desc `find skins -iname onu.xml` >> rc.cpp
-./extract-onu-names.pl  `find skins -iname onu.xml` >> rc.cpp
+$EXTRACTRC `find . -name \*.ui -o -name \*.rc` > rc.cpp
+./extract-onu-names.pl  `find skins -iname world.desktop` >> rc.cpp
 
 $XGETTEXT `find $SRCDIR -name "*.cpp"` rc.cpp -o $podir/ksirk.pot
 rm -f rc.cpp
