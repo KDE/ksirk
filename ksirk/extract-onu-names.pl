4493699d5bb3a1389b54b097724701c2690d0f95
Gaël de Chalendar <kleag@free.fr>
2008-09-19 21:13:49 +0000

    Correct strings extraction scripts
    svn path=/trunk/KDE/kdegames/ksirk/; revision=862789

--- ksirk/extract-onu-names.pl
+++ ksirk/extract-onu-names.pl
@@ -14,11 +14,50 @@ for my $file_name ( @ARGV )
 
   while ( <$fh> )
   {
-    if ($_ =~ /name=(\"[^\"]*\")/)
+    chomp;
+    if ($_ eq "[cannon]"
+      || $_ eq "[cavalry]"
+      || $_ eq "[infantry]"
+      || $_ eq "[exploding]"
+      || $_ eq "[firing]"
+      || $_ eq "[flag]"
+      || $_ eq "[font]"
+      || $_ eq "[onu]"
+      || $_ =~ /\[goal\d+\]/
+      )
+    {
+#       print "$_: next!\n";
+      next;
+    }
+    elsif ($_ =~ /\[([^\]]*)\]/)
     {
       print "//i18n: file $file_name line $.\n";
       print "// xgettext: no-c-format\n";
-      print "i18n($1);\n";
+      print "i18n(\"$1\");\n";
+    }
+    elsif ($_ =~ /^(\w+)=(.*)/)
+    {
+#       print "====== $1  =  $2\n";
+      if ($1 eq "desc" || $1 eq "name")
+      {
+        print "//i18n: file $file_name line $.\n";
+        print "// xgettext: no-c-format\n";
+        print "i18n(\"$2\");\n";
+      }
+      elsif ($1 eq "continents" || $1 eq "countries" || $1 eq "nationalities")
+      {
+        my @strings = split ",", $2;
+        foreach my $str (@strings)
+        {
+          print "//i18n: file $file_name line $.\n";
+          print "// xgettext: no-c-format\n";
+          print "i18n(\"$str\");\n";
+        }
+      }
+    }
+    else
+    {
+#       print "unhandled\n";
     }
   }
 }
