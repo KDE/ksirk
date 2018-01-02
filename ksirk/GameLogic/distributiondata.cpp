0a5df68a665fdbab7d4143a58008851d6277c15d
Gael de Chalendar <gael.de-chalendar@cea.fr>
2018-01-02 13:48:19 +0100

    Update license headers to allow (L)GPL2+

--- ksirk/GameLogic/distributiondata.cpp
+++ ksirk/GameLogic/distributiondata.cpp
@@ -1,7 +1,8 @@
 /*
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
-   License version 2 or later as published by the Free Software Foundation.
+   License either version 2
+   of the License, or (at your option) any later version.or later as published by the Free Software Foundation.
 
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
--- ksirk/iris/src/libidn/internal.h
+++ ksirk/iris/src/libidn/internal.h
@@ -6,7 +6,8 @@
  * GNU Libidn is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * either version 2
+   of the License, or (at your option) any later version.1 of the License, or (at your option) any later version.
  *
  * GNU Libidn is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
